ba55861575551dd538f521a5d5abaa47809395ed
Jake Yang <tsunghan614@gmail.com>
2014-12-01 11:26:16 -0800

    fix my indentation in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,33 @@
 'use strict';
-
 module.exports = function (grunt) {
-
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-jscs');
-
   grunt.initConfig({
     jshint: {
-      all: ['Gruntfile.js', 'index.js', 'class.js', 'student.js'],
-      options: {
-        jshintrc: true
-      }
+      all: [
+        'Gruntfile.js',
+        'index.js',
+        'class.js',
+        'student.js'
+      ],
+      options: { jshintrc: true }
     },
-
     jscs: {
-        all: {
-            options: {
-                // Task-specific options go here.
-            },
-            files: {
-                src: ['index.js', 'class.js', 'student.js'] // Target-specific file/directory goes here.
-            }
+      all: {
+        options: {},
+        files: {
+          src: [
+            'index.js',
+            'class.js',
+            'student.js'
+          ]  // Target-specific file/directory goes here.
         }
+      }
     }
   });
-
-  grunt.registerTask('test', ['jshint', 'jscs']);
+  grunt.registerTask('test', [
+    'jshint',
+    'jscs'
+  ]);
   grunt.registerTask('default', ['test']);
 };
